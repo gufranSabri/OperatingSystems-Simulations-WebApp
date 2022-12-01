cf109a2c0b87698f45b24916cb64a0ea08195c76
Ahmed Hasanaath <61200955+gufranSabri@users.noreply.github.com>
2022-12-01 21:00:47 +0300

    first upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const dotenv= require('dotenv')
+const bodyParser= require('body-parser')
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const app = express()
+
+dotenv.config({path:'./config.env'})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine','pug')
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.get('/',(req,res)=>{res.render("main")})
+app.get("/glm", (req, res)=>{
+    res.send("HELLO FROM GLM")
+})
+
+app.listen(process.env.PORT||8000);
